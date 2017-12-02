6f8c8d43a6a5b258889688ea83b40047af702d74
YouDa LIn <dagekannitiexin@users.noreply.github.com>
2017-12-02 16:42:25 +0800

    12.2

---
+++ XuanMaoShopping/XuanMaoShopping/Classes/个人页面导航/Controller/XMWriterViewController.h
@@ -0,0 +1,13 @@
+//
+//  XMWriterViewController.h
+//  XuanMaoShopping
+//
+//  Created by apple on 2017/12/2.
+//  Copyright © 2017年 林林尤达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMWriterViewController : UIViewController
+
+@end
---
+++ XuanMaoShopping/XuanMaoShopping/Classes/个人页面导航/Controller/XMWriterViewController.m
@@ -0,0 +1,31 @@
+//
+//  XMWriterViewController.m
+//  XuanMaoShopping
+//
+//  Created by apple on 2017/12/2.
+//  Copyright © 2017年 林林尤达. All rights reserved.
+//
+
+#import "XMWriterViewController.h"
+
+@interface XMWriterViewController ()
+
+@end
+
+@implementation XMWriterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //处理navigationBar
+    self.fd_prefersNavigationBarHidden = YES;
+    self.fd_interactivePopDisabled = YES;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
